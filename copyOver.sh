c071b7c3df96f21ac94ca27082ce639ccdc4948e
GpapPeaky <giorgospapamat@gmail.com>
2026-04-26 17:32:53 +0300

    Update

---
+++ copyOver.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+cp $HOME/.xinitrc                    .xinitrc
+cp $HOME/.config/bspwm/*             .config/bspwm
+cp $HOME/.config/polybar/*           .config/polybar/
+cp $HOME/.config/rofi/*              .config/rofi/
+cp $HOME/.config/sxhkd/*             .config/sxhkd/
