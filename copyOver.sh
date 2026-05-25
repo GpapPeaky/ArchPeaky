1d48a1d0261ebf722566ef470fa2c6c9375c035c
GpapPeaky <giorgospapamat@gmail.com>
2026-05-25 16:51:55 +0300

    Made some changes to copy-over script, added the system wide ttf font used

--- copyOver.sh
+++ copyOver.sh
@@ -6,7 +6,8 @@ rsync -av --delete "$HOME/.config/polybar/" .config/polybar/
 rsync -av --delete "$HOME/.config/rofi/" .config/rofi/
 rsync -av --delete "$HOME/.config/sxhkd/" .config/sxhkd/
 rsync -av --delete "$HOME/.config/kitty/" .config/kitty/
+rsync -av --delete "$HOME/.config/MyFonts/" .config/MyFonts/
+rsync -av "$HOME/Scripts/" Scripts/
 rsync -av "$HOME/.xinitrc" .
 rsync -av "$HOME/.bash_profile" .
 rsync -av "$HOME/.bashrc" .
-rsync -av "$HOME/Scripts/" .Scripts/
